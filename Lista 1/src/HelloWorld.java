7a336301b6b42717d19ed73679c8b281e17439b9
Gregory Guths <guthsgregory@gmail.com>
2025-05-09 15:35:13 -0400

    Add HelloWorld class and update .gitignore to exclude VS Code settings

---
+++ Lista 1/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    
+    static void classico() {
+        System.out.println("Hello world"); // Poderia ter feito o hello world simple apenas com o sysout mas usei metodos.
+    }
+    public static void main(String[] args) {
+        classico();
+    }
+}
